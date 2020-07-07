fe9592df1215d23a90d7c5dcf58dc8b49bf08316
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-07 12:27:48 -0700

    Fix day list

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,15 +3,30 @@ import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
+
+const formatSpots = function(num){
+  if (num === 0) {
+    return "no spots remaining";
+  }
+  if (num === 1) {
+    return `${num} spot remaining`;
+  }
+  if (num > 1) {
+    return `${num} spots remaining`;
+  }
+}
+
 export default function DayListItem(props) {
-    const dayClass = classNames("dayList", {
-        "day-list__item--selected": props.selected,
-        "day-list__item--full": !props.spots
-      });
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  const numSpots = formatSpots(props.spots);
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{numSpots}</h3>
     </li>
   );
 }
