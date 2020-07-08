15a22d667d902370dde6e5fa8e42f5463c8a4d2a
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-08 10:50:21 -0700

    Add status story

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,4 +152,5 @@ storiesOf("DayList", module)
     .add("Empty", () => <Empty onAdd={action("onAdd")} />)
     .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
     .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+    .add("Status", () => <Status message="Deleting" />)
     
