ba329e9dd5707c6c53b0d34a77910b5e47b0bf6d
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-08 09:59:34 -0700

    Add appointment story

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
+import Appointment from "components/Appointment";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -134,3 +136,11 @@ storiesOf("DayList", module)
           setInterviewer={action("setInterviewer")}
         />
       ));
+
+    storiesOf("Appointment", module)
+   .addParameters({
+        backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />);
+    
